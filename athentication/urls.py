8520d8c0cf381b36273466bda0ac1d658e5f5748
solaimooon <118716931+solaimooon@users.noreply.github.com>
2024-07-08 00:50:15 +0330

    add reset passwoed page

--- athentication/urls.py
+++ athentication/urls.py
@@ -5,5 +5,6 @@ app_name='athentication'
 
 urlpatterns = [
 path('', login_form, name='log in'),
-path('sign_up/', sign_up_form, name='sign up')
+path('sign_up/', sign_up_form, name='sign up'),
+path('reset_password/',reset_password_form,name='reset_password')
 ]
--- athentication/views.py
+++ athentication/views.py
@@ -6,4 +6,7 @@ def login_form(request):
 
 def sign_up_form(request):
     return render(request,'sign up _ log in/page-signup-simple.html')
+
+def reset_password_form(request):
+    return render(request,'sign up _ log in/page-reset-password-simple.html')
 # Create your views here.
